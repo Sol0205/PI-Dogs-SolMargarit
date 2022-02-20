200b32616446e3dc2654898b1ddf0f1b4ada3f1a
Sol0205 <solmargarit3@gmail.com>
2022-02-20 18:25:27 -0300

    - elimino archivo dupicado - en el controller, devuelvo los temperamentos

--- api/src/endpoints/Temperaments/Temperaments.controller.js
+++ api/src/endpoints/Temperaments/Temperaments.controller.js
@@ -1,15 +1,34 @@
-const axios = require("axios");
-const { Temperament } = require("../../db");
+const axios = require('axios')
+const { Temperament, YOUR_API_KEY } = require("../../db");
+
 
 const temperamentsTypes = async (req, res) => {
-  // const result = await responseAPI
-
   try {
-    const types = await result.data.results.map((type) => type.temperaments);
-    const temperaments = types.flat();
-    const typeTemperaments = [...new Set(temperaments)];
+    // hago un request
+    const { data: perros } = await axios.get(`https://api.thedogapi.com/v1/breeds?apiKey=${YOUR_API_KEY}`);
 
-    typeTemperaments.forEach((temperament) => {
+
+    // mapeo los temperamentos
+    const temperaments = perros
+      .map(function (dog) {
+        // me devuelve un string con varios temperamentos
+        // .split = dividido la cadena de texto por una coma
+        // .trim = elimina los espacios en blanco en ambos extremos del string
+        // temperament? = si el temperamento me devuelve null o undefined me lo devuelve en la consola
+        return dog.temperament?.split(',').map(s => s.trim());
+      })
+      // .flat = aplano un array ya que devuelve un array de arrays
+      // .filter = lo utilizo para que me devuelva el mismo valor como true o false, en este caso necesito todos true
+      .flat()
+      .filter(e => e)
+    // ['', '', '',]
+
+    // crea un array sin elementos duplicados(temperamentos)
+    const temperamentsWithoutDuplicates = [...new Set(temperaments)];
+    console.log('temperamentsWithoutDuplicates: ', temperamentsWithoutDuplicates);
+
+    // ejecuto una vez por elementos del arrar
+    temperamentsWithoutDuplicates.forEach((temperament) => {
       Temperament.findOrCreate({
         where: { name: temperament },
       });
@@ -18,7 +37,9 @@ const temperamentsTypes = async (req, res) => {
     const allTemperaments = await Temperament.findAll();
     res.status(200).json(allTemperaments);
   } catch (err) {
-    console.log(err);
+    res.json({ err });
+    console.log(err)
+    throw err
   }
 };
 
