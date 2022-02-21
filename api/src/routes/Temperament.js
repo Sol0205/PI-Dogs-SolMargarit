fd61cd3ae8b932f5052d97c82a2b83007526cb32
Sol0205 <solmargarit3@gmail.com>
2022-02-20 21:58:54 -0300

    - reacomodamiendo de archivos

--- api/src/routes/Temperament.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-const  { temperamentsTypes } = require('../controllers/Temperament')
-
-router.get('/temperamentsTypes', temperamentsTypes)
-
-module.exports = router
--- api/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const dogsRouter = require('../endpoints/Dogs/Dogs.router')
-const temperamentRouter = require('./Temperament')
-
-router.use('/dogs', dogsRouter)
-router.use('/temperaments', temperamentRouter)
-
-module.exports = router;
