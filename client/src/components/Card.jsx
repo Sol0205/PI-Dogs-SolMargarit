e347d8366f2f27a40e32a874bc3bf6f95dcbfde6
Sol0205 <solmargarit3@gmail.com>
2022-02-25 22:07:29 -0300

    -

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './styles/Card.css'
 
 export default function Card({ name, weight, height, image}) {
     return (
-        <div className="">
-            <p className="">{name}</p>
-            <p>Peso: {weight.imperial}</p>
-            <p>Altura: {height.imperial}</p>
+        <div className="container-card">
             <img src={image.url} alt="img not found" width="200px" height="250px" />
+            <h3 className="title">{name}</h3>
+            <p className="info-card">Peso: {weight.imperial} Kg</p>
+            <p className="info-card">Altura: {height.imperial} Cm</p>
+            <a href="d etall">Leer Màs</a>
         </div>
     )
 }
